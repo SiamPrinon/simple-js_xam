4d18427bbb307fb91a9bb13c13ff12cbf50ea84b
SiamPrinon <siamprinon1710@gmail.com>
2023-12-13 23:55:55 +0600

    simple calculation with js

---
+++ app.js
@@ -0,0 +1,121 @@
+//prob 1
+
+function num (Fnum, Snum){
+    return sum = (Fnum + Snum);
+}
+
+console.log(num(5, 6));
+
+
+//prob 2
+
+function conv (min){
+    return sec = (min * 60);
+}
+
+console.log(conv(6));
+
+//prob 3
+
+function n (numm){
+    return sum = (numm + 1);
+}
+
+console.log(n(7));
+
+
+//prob 4
+
+function tArea (base, height){
+    return area = ((base*height)/2);
+}
+
+console.log(tArea(5, 6));
+
+
+//prob 6
+
+function Age (age){
+    return result = (age * 365);
+}
+
+console.log(Age(5));
+
+//prob 7
+
+function power (v, c){
+    return cPower = (v * c);
+}
+
+console.log(power(100, 6));
+
+//prob 8
+
+function edge (Fside, Sside){
+    return mRange = ((Fside + Sside) -1);
+}
+
+console.log(edge(13, 6));
+
+//prob 9
+
+function rem (Fnum, Snum){
+    return resultt = (Fnum % Snum);
+}
+
+console.log(rem(5, 6));
+
+//prob 10
+
+function str (x){
+    return resulttt = ("something" + x);
+}
+
+console.log(str("is fishy"));
+console.log(str("is wrong"));
+
+//prob 11
+
+function sqMath (a){
+    return value = (a * a);
+}
+
+console.log(sqMath(6));
+
+//prob 12
+
+function hello (number){
+    return valuee = (number <= 0);
+}
+
+console.log(hello(6));
+
+
+//prob 13
+
+function poly (b){
+    return sol = ((b - 2) * 180);
+}
+
+console.log(poly(6));
+
+
+//prob 14
+
+function nStr (name){
+    var r = name + "coder";
+    return r;
+}
+
+console.log(nStr("Ronaldo"));
+
+//prob 15
+
+function less (n1, n2){
+    return sumValue = ((n1 + n2) < 100);
+}
+
+console.log(less(6, 7));
+console.log(less(150, 7));
+
+
